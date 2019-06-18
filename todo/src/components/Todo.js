0b4a5c4a72b43c73181928ce05b56219ab36cc29
Josemaria <josemarianriagu25@gmail.com>
2019-06-18 22:20:59 +0800

    Disable buttons while actively editing

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,26 +1,32 @@
 import React from 'react';
 
-const Todo = props => {
+const Todo = ({ todo, index, onEdit, setEdit, deleteTodo, markCompleted }) => {
 
     const style = {
         cursor: "pointer",
         // Add style if completed
-        textDecoration: (props.todo.completed) ? "line-through" : "initial",
-        color: (props.todo.completed) ? "gray" : "initial"
+        textDecoration: (todo.completed) ? "line-through" : "initial",
+        color: (todo.completed) ? "gray" : "initial"
     };
+    // disables the buttons if todo is being actively edited
+    const disabled = onEdit === todo.id ? true : false;
 
     return (
         <div className='todo'>
-            <h3>Todo: {props.index + 1}</h3>
+            <h3>Todo: {index + 1}</h3>
             <div
                 style={style}
-                onClick={() => props.markCompleted(props.todo.id)}
-            >{props.todo.task}</div>
+                onClick={() => markCompleted(todo.id)}
+            >{todo.task}</div>
             <div className='actions'>
-                <button onClick={''}>
+                <button
+                    disabled={disabled}
+                    onClick={(e) => setEdit(e, todo.task, todo.id)}>
                     Edit
                 </button>
-                <button onClick={() => props.deleteTodo(props.todo.id)}>
+                <button
+                    disabled={disabled}
+                    onClick={() => deleteTodo(todo.id)}>
                     Delete
                 </button>
             </div>
