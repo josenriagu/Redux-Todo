83a1a56a25728a3cedfd930216d054be25136f93
Josemaria <josemarianriagu25@gmail.com>
2019-06-18 22:48:41 +0800

    [Stretch] persist Redux Store data to localStorage

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { reducer } from './reducer'
+import { reducer } from './reducer';
+import { loadState, saveState } from './localStorage.js';
 import './index.css';
 import App from './App';
 
+const persistedState = loadState();
 const store = createStore(
-    reducer, /* preloadedState, */
+    reducer,
+    persistedState,
+    /* preloadedState, */
     // this line below makes it possible to use the ReduxDevTool on the browser
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
+
+// saves a copy of store to localStorage upon change
+store.subscribe(() => {
+    saveState(store.getState())
+})
 
 ReactDOM.render(
     <Provider store={store}>
