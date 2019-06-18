0dadd3d51d6028e5b877b6232eeab174b36fea58
Josemaria <josemarianriagu25@gmail.com>
2019-06-18 15:46:03 +0800

    Define initialState Reduce state slices for each action type

---
+++ todo/src/reducer/index.js
@@ -0,0 +1,49 @@
+import { ADD_TODO, DELETE_TODO, MARK_COMPLETED } from '../actions';
+
+
+// A reducer is just a state tree that can be used to recreate new state trees when triggered by actions
+
+const initialState = {
+    todoList: [
+        {
+            task: 'Learn Redux',
+            id: 0,
+            completed: false
+        }
+    ],
+}
+
+// state = initialState gives a fallback value for when the component renders without the state
+// switch tests for each type of the action creator function and maps them onto cases
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ADD_TODO:
+            return {
+                ...state,
+                todoList: [...state.todoList, action.payload] //action has type and payload. the payload holds the state changing value for each accompanying action.
+            };
+        case DELETE_TODO:
+            return {
+                ...state,
+                todoList: state.todoList.filter(todo => todo.id !== action.payload.id)
+            }
+        case MARK_COMPLETED:
+            return {
+                ...state,
+                todoList: state.todoList.map(todo =>
+                {
+                    if (todo.id === action.payload.id) {
+                        return {
+                            id: todo.id,
+                            task: todo.task,
+                            completed: !todo.completed
+                        }
+                    }
+                    return todo;
+                })
+            }
+        default:
+            return state;
+    }
+}
